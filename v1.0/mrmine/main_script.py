01820268d880a6cf6f7bf8ae3291b2c417446a25
maple_ <102273486+XtheGxmerz0@users.noreply.github.com>
2024-10-22 16:16:57 -0700

    Update main_script.py

--- v1.0/mrmine/main_script.py
+++ v1.0/mrmine/main_script.py
@@ -7,39 +7,40 @@ import os
 key_pressed = None
 
 def keypress_listener():
-				global key_pressed
-				while True:
-								key = detect_keypress_nonblocking()
-								if key:
-												key_pressed = key.lower()
-												if key_pressed == '\x03':  # Ctrl+C for interrupt
-																raise KeyboardInterrupt
+	global key_pressed
+	while True:
+		key = detect_keypress_nonblocking()
+		if key:
+			key_pressed = key.lower()
+			if key_pressed == '\x03':  # Ctrl+C for interrupt
+				raise KeyboardInterrupt
 
 def tick():
-				global key_pressed
-				while True:
-								if key_pressed:
-												key = key_pressed
-												key_pressed = None
-												print(f"Key registered: {key}")
-												if key == 'up' or key == 'down':
-																scroll(key)
-																update_GUI()
-												elif key in ["k", "q", "c", "s", "h", "u", " ", "p", "r"]:
-																update_GUI_func(key)
+	global key_pressed
+	while True:
+		if key_pressed:
+			key = key_pressed
+			key_pressed = None
+			print(f"Key registered: {key}")
+			if key == 'up' or key == 'down':
+				scroll(key)
+				update_GUI()
+			elif key in ["k", "q", "c", "s", "h", "u", " ", "p", "r"]:
+				update_GUI_func(key)
 
 def mrmine_start_game():
-				os.system('clear')
-				gui_init()
-				update_GUI()
+	os.system('clear')
+	update_GUI()
+	try:
+		
 
-				try:
-								listener_thread = Thread(target=keypress_listener)
-								listener_thread.daemon = True
-								listener_thread.start()
+	try:
+		listener_thread = Thread(target=keypress_listener)
+		listener_thread.daemon = True
+		listener_thread.start()
 
-								while True:
-												tick()
+		while True:
+			tick()
 
-				except KeyboardInterrupt:
-								print("Game interrupted, exiting...")
+	except KeyboardInterrupt:
+		print("Game interrupted, exiting...")
