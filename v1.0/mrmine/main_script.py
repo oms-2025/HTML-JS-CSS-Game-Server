80d6cd25c01377a9ca3ee0d397b509428434b79f
maple_ <102273486+XtheGxmerz0@users.noreply.github.com>
2024-10-22 15:56:58 -0700

    Create main_script.py for mrmine

---
+++ v1.0/mrmine/main_script.py
@@ -0,0 +1,45 @@
+from threading import Thread
+from mrmine_keypress_detector import detect_keypress_nonblocking
+from mrmine_gui import update_GUI, scroll, update_GUI_func
+import os
+
+# Make the relay of keys more accessible
+key_pressed = None
+
+def keypress_listener():
+				global key_pressed
+				while True:
+								key = detect_keypress_nonblocking()
+								if key:
+												key_pressed = key.lower()
+												if key_pressed == '\x03':  # Ctrl+C for interrupt
+																raise KeyboardInterrupt
+
+def tick():
+				global key_pressed
+				while True:
+								if key_pressed:
+												key = key_pressed
+												key_pressed = None
+												print(f"Key registered: {key}")
+												if key == 'up' or key == 'down':
+																scroll(key)
+																update_GUI()
+												elif key in ["k", "q", "c", "s", "h", "u", " ", "p", "r"]:
+																update_GUI_func(key)
+
+def mrmine_start_game():
+				os.system('clear')
+				gui_init()
+				update_GUI()
+
+				try:
+								listener_thread = Thread(target=keypress_listener)
+								listener_thread.daemon = True
+								listener_thread.start()
+
+								while True:
+												tick()
+
+				except KeyboardInterrupt:
+								print("Game interrupted, exiting...")
