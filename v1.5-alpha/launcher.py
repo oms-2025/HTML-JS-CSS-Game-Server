abef45ad117f0a1bfe2fa50c9047ec5f20046d1b
maple_ <102273486+XtheGxmerz0@users.noreply.github.com>
2024-11-06 09:59:53 -0800

    v1.5-alpha upload

---
+++ v1.5-alpha/launcher.py
@@ -0,0 +1,44 @@
+import time, os, random
+from mrmine.main_script import mrmine_start_game, m_ms_initialize
+from mrmine.keypress_detector import m_kd_initialize
+from mrmine.gui import m_g_initialize
+from clicker.main_script import clicker_start_game
+from mrmine_integrity import m_ic_initialize
+from colorama import Fore as color
+os.system('clear')
+m_version='v1.5-alpha'
+print(f'''
+You are on version {m_version}.
+To see all versions go to:
+https://github.com/XtheGxmerz0/coolmathgames''')
+time.sleep(0.75)
+print('''
+Enter the ID for the game you would like to play.
+1. Mr. Mine                            | [v1.5-alpha, Work-In-Progress]
+2: Clicker Game                        | [v1.0, Work-In-Progress]''')
+game=input(" > ")
+try:
+	game=int(game)
+except Exception:
+	print("Invalid ID.")
+	exit()
+if game==1:
+	print("Starting Mr. Mine...")
+	time.sleep(0.5)
+	print("Loading Game...")
+	time.sleep(random.uniform(0.5, 1.5))
+	os.system('clear')
+	print('Initializing main script...')
+	m_ms_initialize()
+	print('Initializing GUI...')
+	m_g_initialize()
+	print('Initializing keypress detector...')
+	m_kd_initialize()
+	print('Performing integrity check...')
+	m_ic_initialize()
+	print("Loading Complete!")
+	time.sleep(1)
+	print("Welcome to Mr. Mine!")
+	time.sleep(0.5)
+	os.system('clear')
+	mrmine_start_game()
---
+++ v1.5-alpha/mrmine/keypress_detector.py
@@ -0,0 +1,47 @@
+import sys
+import termios
+import tty
+import select
+import time
+from contextlib import contextmanager
+log_loc= '/workspaces/coolmathgames/v1.5-alpha/mrmine/log.txt'
+def write_log(message):
+	with open(log_loc, "a") as log_file:
+		message1=('Timestamp '+ str(time.time())+': '+ str(message) + "\n")
+		log_file.write(message1)
+def m_kd_initialize():
+	write_log('From function \'initialize\': Initializing script...')
+@contextmanager
+def raw_mode(file):
+	old_attrs = termios.tcgetattr(file.fileno())
+	try:
+		tty.setraw(file.fileno())
+		yield
+	finally:
+		termios.tcsetattr(file.fileno(), termios.TCSADRAIN, old_attrs)
+
+SPECIAL_KEYS = {
+	'\x1b[A': 'UP',
+	'\x1b[B': 'DOWN',
+	'\x1b[C': 'RIGHT',
+	'\x1b[D': 'LEFT'
+}
+
+def detect_keypress_nonblocking():
+	with raw_mode(sys.stdin):
+		rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+		if rlist:
+			chars = []
+			char = sys.stdin.read(1)
+			if char == '\x1b':  # Escape character
+				chars.append(char)
+				char = sys.stdin.read(1)
+				if char: 
+					chars.append(char)
+					if char == '[':
+						char = sys.stdin.read(1)
+						if char:
+							chars.append(char)
+			result = ''.join(chars)
+			return SPECIAL_KEYS.get(result, result)
+	return None
