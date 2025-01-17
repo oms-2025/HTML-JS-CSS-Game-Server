c03fa3b4b1034e858af04b0a6922652a963896ff
maple_ <102273486+XtheGxmerz0@users.noreply.github.com>
2025-01-17 17:22:52 +0000

    add maintenance mode

--- server-1.2/main/server.js
+++ server-1.2/main/server.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d5f072ea15888bb0cf938a0d271aa06dd9795f90c50e021ec6a7325b2748720
-size 8617
+oid sha256:13fec58d6d37e0463aab88be1228f5858345d34afca1d2eee2544798d0e70a62
+size 8794
